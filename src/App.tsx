50879985194e4fcf862da335d2d1796800e45bab
zhuhui_yq <kyrie890514@163.com>
2022-10-13 17:09:22 +0800

    single html

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { defineComponent } from 'vue'
-import { RouterView } from 'vue-router'
+import WordsMemorize from './views/WordsMemorize'
 
 export default defineComponent({
 	render() {
-		return <RouterView />
+		return <WordsMemorize />
 	}
 })
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import { createApp } from 'vue'
 
 import App from './App'
-import router from './router'
 import './style/main.scss'
 
-
 const app = createApp(App)
-
-app.use(router)
 
 app.mount('#app')
