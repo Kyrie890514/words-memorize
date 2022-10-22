7ae613a77103a17d13b9bf291f5a137a29ca7a2b
Kyrie890514 <kyrie890514@163.com>
2022-10-22 18:38:21 +0800

    list 1 group 4

--- src/data/help.js
+++ src/data/help.js
@@ -1,20 +1,23 @@
-const group = 'Group3'
+const group = 'Group4'
 
 const string = `
-|abjure|a claim|发誓放弃|
-|abnegate|the resultless love|放弃|
-|abnormal|phenomena|异常|
-|abolish|an old tradition|废除|
-|abominate|the code weather|厌恶|
-the wave of |abominable|crimes|恶劣的|
-|aborted|babies|流产的|
-|abort |her travelling plan|终止|
-an|abortive|plan|失败的|
-|abound|with confidence|充满|
-|abundant|cash|充足的
+her|aboveboard|business|正大光明的|
+|abrade|her shin|擦伤|
+his|abrasive|managing style|粗暴的|
+the | abrasive |surfaca| 粗糙的|
+|walk |abreast|	齐头|
+|abridged|books|删减的|
+|abrogate|a law|废除|
+|abrupt|changes of weather|突然|
+|absconded|prisoners|潜逃的|
+|absent|from school|不在|
+his |absent|mind|心不在焉
 `
 
 const result = { [group]: [] }
+if (string.indexOf('｜') > 0) {
+	throw new Error('existed ｜')
+}
 string.split('|').map(v => v.trim()).reduce((pre, cur, index) => {
 	switch (index % 4) {
 		case 0:
