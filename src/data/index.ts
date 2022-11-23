f32c8bd46857bf5282abb6199197d793b453c227
zhuhui <kyrie890514@163.com>
2022-11-23 15:03:26 +0800

    dist

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,10 +1,10 @@
 import type { List } from './type'
 
 import List1 from './List1.json'
-import List2 from './List2.json'
+// import List2 from './List2.json'
 import List13 from './List13.json'
 
 
 export const lists: Record<string, List> = {
-	List1, List2, List13
+	List1, List13
 } 
