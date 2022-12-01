4a8ee0447a60175266dcbd497515e78ea274acab
zhuhui <kyrie890514@163.com>
2022-12-01 09:34:33 +0800

    dist

--- src/data/index.ts
+++ src/data/index.ts
@@ -4,7 +4,10 @@ import List1 from './List1.json'
 // import List2 from './List2.json'
 import List13 from './List13.json'
 
+import Frontend from './Frontend.json'
+
 
 export const lists: Record<string, List> = {
-	List1, List13
+	List1, List13,
+	Frontend
 } 
