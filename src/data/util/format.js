07a54d20c114f48855b7ada714afefd077ca31c8
zhuhui <kyrie890514@163.com>
2022-12-26 10:07:03 +0800

    rename

---
+++ src/data/util/format.js
@@ -0,0 +1,38 @@
+// const string = `
+// `
+
+// Frontend
+const string = `
+| the method is straightforward but |cumbersome|麻烦的|
+each |daemon | has its own log file|守护进程|
+the |objective|of the game|目的|
+`
+
+const result = []
+if (string.indexOf('｜') > 0) {
+	throw new Error('There are some ｜ existed.')
+}
+const a = string.split('|')
+if ((a.length - 1) % 4 !== 0) {
+	throw new Error(`The count of '|' is not a multiple of 4.`)
+}
+a.map(v => v.trim()).reduce((pre, cur, index) => {
+	switch (index % 4) {
+		case 0:
+			pre = { left: cur }
+			break
+		case 1:
+			pre.middle = cur
+			break
+		case 2:
+			pre.right = cur
+			break
+		case 3:
+			pre.meaning = cur
+			pre.phonogram = '//'
+			result.push(pre)
+			break
+	}
+	return pre
+}, {})
+console.log(JSON.stringify(result).slice(1, -1))
